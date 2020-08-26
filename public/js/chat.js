214e3551e1462e2c1412505c32c35796745cab38
Sparth19 <parthshekhaliya18@gmail.com>
2020-08-26 15:56:23 +0530

    Join page,Chat page with backend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,68 @@
 const socket = io()
 
-socket.on('message', (msg) => {
-    console.log(msg)
+//Elements
+const msgForm = document.querySelector('#msgform')
+const msgSendBtn = document.querySelector('#send')
+const text = document.getElementById('inputtext')
+const locSendBtn = document.getElementById('send-location')
+const message = document.getElementById('message')
+
+//Templates
+const messageTemplate = document.getElementById('message-template').innerHTML
+const locationTemplate = document.getElementById('location-template').innerHTML
+
+socket.on('message', (msgObject) => {
+    console.log(msgObject)
+    const html = Mustache.render(messageTemplate, {
+        message: msgObject.text,
+        createdAt: moment(msgObject.createdAt).format('h:mm a')
+    })
+    message.insertAdjacentHTML('beforeend', html)
+
 })
 
-document.querySelector('#msgform').addEventListener('submit', (e) => {
+socket.on('locationMessage', (locMsgObject) => {
+    console.log(locMsgObject)
+    const html = Mustache.render(locationTemplate, {
+        url: locMsgObject.url,
+        createdAt: moment(locMsgObject.createdAt).format('h:mm a')
+    })
+    message.insertAdjacentHTML('beforeend', html)
+})
+
+msgForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const text = document.getElementById('inputtext')
-    socket.emit('sendmsg', text.value)
+        //disable btn
+    msgSendBtn.setAttribute('disabled', 'disabled')
+    socket.emit('sendmsg', text.value, (error) => {
+        //enable
+        msgSendBtn.removeAttribute('disabled')
+        text.value = ''
+        text.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log('msg delivered..')
+    })
+})
+
+locSendBtn.addEventListener('click', () => {
+
+
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser..')
+    }
+    //disable
+    locSendBtn.setAttribute('disabled', 'disabled')
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        }, (msg) => {
+            //enable
+            locSendBtn.removeAttribute('disabled')
+            console.log(msg)
+        })
+    })
 })
--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const path = require('path')
-
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 const port = process.env.PORT
 
 const publicPath = path.join(__dirname, '../public')
@@ -16,13 +17,31 @@ app.use(express.static(publicPath))
 let count = 0
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
-    socket.emit('message', "Welcome !") //emits to single connection
+    socket.emit('message', generateMessage("Welcome !")) //emits to single connection
+    socket.broadcast.emit('message', generateMessage('A new user has joined..'))
 
-    socket.on('sendmsg', (msg) => {
-        io.emit('message', msg) //emits to all connections
+
+    socket.on('sendmsg', (msg, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(msg)) {
+            return callback('Profane words not allowed')
+        }
+        io.emit('message', generateMessage(msg)) //emits to all connections
+        callback()
+    })
+
+    socket.on('sendLocation', ({ lat, long }, callback) => {
+        io.emit('locationMessage', generateLocationMessage("https://www.google.com/maps?q=" + lat + "," + long))
+        callback('Location shared..')
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left..'))
     })
 
 })
+
+
 
 server.listen(port, () => {
     console.log('Server is on | ' + port)
